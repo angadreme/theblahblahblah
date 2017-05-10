6ffb5ac5cb2c61a1ab12ca1cd33a7bc6119b7211
Rosa Andrews <rosamaya.27@hotmail.com>
2017-05-10 11:08:01 -0700

    Need to add add() to api and services.

---
+++ api/courses.ts
@@ -0,0 +1,29 @@
+import * as express from 'express';
+import Course from '../models/course';
+
+let router = express.Router();
+
+router.get('/', (req, res) => {
+  Course.find().then((courses) => res.json(courses));
+});
+
+router.post('/', (req, res) => {
+  let newCourse = new Course();
+  newCourse.name = req.body.name;
+  newCourse.icon = req.body.icon;
+});
+
+router.get('/:id', (req, res) => {
+  Course.findById(req.params.id)
+  .then((foundCourse) => res.json(foundCourse));
+});
+
+router.post('/:id', (req, res) => {
+  Course.findById(req.params.id).then((foundCourse) => {
+    foundCourse.name = req.body.name;
+    foundCourse.icon = req.body.icon;
+    foundCourse.save().then((savedCourse) => res.json(savedCourse));
+  });
+});
+
+export default router;
